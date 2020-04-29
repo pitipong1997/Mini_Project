790674ba04303f0d9cccd1d75196381d89d86ad1
pitipong1997 <5835512033@psu.ac.th>
2020-04-29 17:23:45 +0700

    Update

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -39,13 +39,6 @@ class ImageUpload extends Component {
             console.log(url);
             this.setState({url});
         })
-    },
-    () => {
-      storage.ref('images').child(image.name).listAll().then(function(result){
-        result.items.forEach(function(imageRef){
-          console.log("Image reference" + imageRef.toString());
-        })
-      })
     }
     );
   }
